a1a3d8b75b26dc06ab5621351419d8ff2aa7669e
radovandelic <r.delic@net.hr>
2020-10-10 11:47:08 +0200

    task 5 done

--- src/05_overlapping_spreads/index.js
+++ src/05_overlapping_spreads/index.js
@@ -5,6 +5,51 @@
 // resulting array after replacement would be -4,-2,4,6,7,9,-2,0,5,7,10,12 and duplicate numbers are -2,7 and the 
 // result will be 5.
 
-exports.overlappingSpreads = function(data) {
-    //TODO implement me
+const findDuplicates = data => {
+    const negativeCounts = []
+    const positiveCounts = []
+    const duplicates = []
+    const max = Math.max(...data)
+    let min = Math.min(...data)
+    for (let i = 0; i <= max; i++) {
+        positiveCounts[i] = 0
+    }
+
+    if (min < 0) {
+        min = -min
+        for (let i = 0; i <= min; i++) {
+            negativeCounts[i] = 0
+        }
+    }
+
+    for (const number of data) {
+        if (number < 0) {
+            negativeCounts[-number]++
+        } else {
+            positiveCounts[number]++
+        }
+    }
+    for (const number in negativeCounts) {
+        if (negativeCounts[number] > 1) {
+            duplicates.push(-number)
+        }
+    }
+    for (const number in positiveCounts) {
+        if (positiveCounts[number] > 1) {
+            duplicates.push(number)
+        }
+    }
+    return duplicates
 }
+
+exports.overlappingSpreads = function (data) {
+    const newArray = []
+    for (const number of data) {
+        newArray.push(number - 1)
+        newArray.push(number + 1)
+    }
+    const duplicates = findDuplicates(newArray)
+    return duplicates.reduce((a, b) => Number(a) + Number(b), 0)
+}
+
+//overlappingSpreads([-3, 5, 8, -1, 6, 11])
